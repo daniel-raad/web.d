5848e9d7ef83ab7b714f1b12278e59fe27d9637a
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-01-29 11:28:02 +0000

    Initial code for about page

---
+++ components/Description.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Description.module.css";
+
+export default function Description(){ 
+    return ( 
+        <div className={styles.about}>
+            <h2>About</h2>
+            <p> Hello there! My name is Daniel and its lovely to meet you! </p>
+            <p> On this website, its going to be a omnishambles (to put it politely) of different things i try out! Please forgive the trainwreck </p>
+            <p> i am currently working on a number of different projects, including this website, as i learn react and next, along with GCP and a host of other technologies</p>
+            <p> My bread and butter really is python, where at the Uni of Southampton I specialised in machine learning - my projects will be posted under projects, or you can find my cv at the bottom of this page :) </p>
+            Thank you for joining me on this journey through the wild west of technology
+        </div>
+    )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+import Header from "../components/Header"
+import Description from "../components/Description"
+import { Card } from "react-bootstrap"
+import styles from "../styles/About.module.css";
 
 
 export default function About() {
@@ -9,6 +13,13 @@ export default function About() {
         <meta name="description" />
         <link rel="icon" href="/NinjaEmoji.jpeg" />
       </Head>
+      <Card className={styles.card}>
+        <Card.Body>
+          <Header/>
+          <Description />
+        </Card.Body>
+      </Card>
+
     </>
   )
 }
