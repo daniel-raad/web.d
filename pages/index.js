67fd12bd4029889a7b3de1c26ea7b74f5fc56ae5
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-01-31 23:10:37 +0000

    Authentication with github added at /auth

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import MyTerminal from '../components/MyTerminal'
- import styled from 'styled-components';
+import styled from 'styled-components';
+import {auth, app} from "../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Hero = styled.div`
   height: 15vh;
@@ -17,8 +19,9 @@ const Heading = styled.h1`
 `;
 
 
+
 export default function Home() {
-  return (
+  return (    
     <>
       <Head>
         <title>draad is typing...</title>
