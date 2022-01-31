caeed92b579de7562a1c95eea69c12954598f693
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-01-31 23:52:18 +0000

    Updated credentials

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyB4fes94TPN-lcqwQ0R2CJ7QwJS5Me-QF0",
+    authDomain: "danielsweb3d.firebaseapp.com",
+    projectId: "danielsweb3d",
+    storageBucket: "danielsweb3d.appspot.com",
+    messagingSenderId: "986252284307",
+    appId: "G-T3F6G2PT4E",
 };
 
 // Initialize Firebase
