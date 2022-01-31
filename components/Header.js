c8d1f95fb66b19455287aad86c24d442726b8f49
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-01-31 23:37:28 +0000

    fixed about page

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,10 @@ import Image from 'next/image'
 export default function Header(){ 
     return(
         <main className={styles.header}>
-            <Image src="/NinjaEmoji.jpeg" className={styles.dp} />
+
+            <Image src="/NinjaEmoji.jpeg" width="200" height="200" className={styles.dp} />
+
+            
             <h1 className={styles.name}> Daniel Raad</h1>
             <p className={styles.about}> 
                 Software Engineer at Lloyds Banking Group 
