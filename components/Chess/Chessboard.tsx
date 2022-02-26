3fabc25a13589b16cc2457b6d6067c304a8ae591
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-02-26 15:19:21 +0000

    Board with pieces, next step to load them correctly, then give functionality

--- components/Chess/Chessboard.tsx
+++ components/Chess/Chessboard.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import Tile from './Tile';
 
+
+interface Piece { 
+  image: string; 
+  x: number; 
+  y: number; 
+}
+
+const pieces: Piece[] = []; 
+const initialFen = " "
+//TODO: have an approach which correctly adds pieces to the board 
+
+
 const maxFile = 8;
-const verticalAxis = ['1','2','3','4','5','6','7','8'];
-const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const maxRank = 8;
 
+
 export default function Chessboard() { 
+
     let board = []; 
     let count = 0 
     for(let j = maxFile - 1; j >= 0 ; j--){ 
         count += 1
         for(let i = 0; i < maxRank; i++){
             count += 1 
-            board.push(<Tile count={count}/>)
-            
+            board.push(<Tile count={count} piece={"king_w.png"}/>)
         }
     }
   return (
-    <div className='grid grid-cols-8 grid-rows-8  w-[50rem] h-[50rem]'>
+    <div className='grid grid-cols-8 grid-rows-8 w-[50rem] h-[50rem]'>
         {board}
     </div>
   )
--- components/Chess/Tile.tsx
+++ components/Chess/Tile.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
-
+import Image from 'next/image'
 // An interface is used to ensure that the element being passed through 
 // is strongly typed 
 
 interface Props { 
     count: number
+    piece: string
 }
 
-const Tile = ({count}: Props) => {
+const Tile = ({piece, count}: Props) => {
     if(count % 2 === 0){ 
-       return <div className=' w-[6.25rem] h-[6.25rem] bg-white'></div>
+       return (
+            <div className='flex w-[6.25rem] h-[6.25rem] bg-white justify-center items-center'>
+                <div className="w-[4rem] h-[4rem]"> 
+                    <Image src="/chessImages/king_b.png" width="100" height="100"/>
+                </div> 
+            </div>
+       )
     } else { 
-       return <div className=' w-[6.25rem] h-[6.25rem] bg-green-500'> </div>
+        return (
+            <div className='flex w-[6.25rem] h-[6.25rem] bg-green-500 justify-center items-center'>
+                <div className="w-[4rem] h-[4rem]"> 
+                    <Image src={`/chessImages/${piece}`} width="100" height="100"/>
+                </div> 
+            </div>
+       )
     }
 }
 
