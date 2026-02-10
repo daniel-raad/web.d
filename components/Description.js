f46a8612bff4018ecafe0db2137132d88605fd99
Daniel Raad <daniel_raad@hotmail.co.uk>
2026-02-10 22:41:12 +0000

    Update doc

--- components/Description.js
+++ components/Description.js
@@ -4,9 +4,8 @@ export default function Description(){
     return ( 
         <div className={styles.about}>
             <p> Hello there, my name is Daniel and its lovely to meet you :) </p>
-            <p> Feel free to browse this website as I add new elements and mess around with new things.</p>
+            <p> I am a Forward Deployed Operations Engineer at Palantir.</p>
             <p> I am currently working on a number of different projects, some of which you can find under /projects.</p>
-            <p> My bread and butter is python, where at the Uni of Southampton I specialised in machine learning - check out my cv at the bottom of this page :) </p>
         </div>
     )
 }
