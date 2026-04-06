978a23fbb0d13932ca8b6af1cb3c063f0814f8bb
Daniel Raad <daniel_raad@hotmail.co.uk>
2026-04-06 20:38:04 +0100

    new cv

--- pages/api/cv.js
+++ pages/api/cv.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end()
 
   const apiKey = process.env.GOOGLE_API_KEY
-  const fileId = "1GCafwrJ3yx5lKcNySPHfUQioyYHf1h-jkFNk9KaRYws"
+  const fileId = "1Fqr19c6MVdTS9W97izyD8cykJ2rVVFvm3hyWM5ichIo"
   const url = `https://www.googleapis.com/drive/v3/files/${fileId}/export?key=${apiKey}`
 
   const response = await axios.get(url, {
